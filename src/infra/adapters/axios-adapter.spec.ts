b3e3cd80096aeadda6cbed7144f2cfa92801a8d4
Rafael Gomes <rafael.gcaldas01@gmail.com>
2024-11-06 11:21:03 -0300

    test: ensure AxiosClientAdapterreturns correct values

--- src/infra/adapters/axios-adapter.spec.ts
+++ src/infra/adapters/axios-adapter.spec.ts
@@ -4,9 +4,26 @@ import { AxiosHttpClientAdapter } from './axios-adapters';
 
 import axios from 'axios';
 import { HttpRequest } from './axios-adapters';
+import type { User } from '../../@types';
+
+const createRandomUser = (): User => {
+  return {
+    id: faker.number.int(),
+    email: faker.internet.email(),
+    name: faker.person.firstName(),
+    username: `${faker.person.firstName()} ${faker.person.lastName()}`,
+  };
+}
+
+const mockedAxiosResult = {
+  data: createRandomUser(),
+  status: faker.number.int()
+}
 
 vi.mock('axios')
 const mockedAxios = axios as Mocked<typeof axios>;
+
+mockedAxios.request.mockResolvedValue(mockedAxiosResult)
 
 const makeSut = () => {
   return new AxiosHttpClientAdapter()
@@ -30,4 +47,17 @@ describe('AxiosHttpClientAdapter', () => {
 
     expect(mockedAxios.request).toHaveBeenCalledWith(requestParams)
   })
+
+  it('should return the correct statusCode and body', async () => {
+    const sut = makeSut();
+
+    const httpResponse = await sut.request(mockGetRequest())
+
+    expect(httpResponse).toEqual({
+      statusCode: mockedAxiosResult.status,
+      body: mockedAxiosResult.data
+    })
+  })
+
+
 })
