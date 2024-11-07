fd964d0d37b276fe349ab403369d1d1e5a36c9a4
Rafael Gomes <rafael.gcaldas01@gmail.com>
2024-11-07 14:55:54 -0300

    refactor: refactor load user list

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import { UserList } from "./components/user/user-list";
+import type { HttpRequestParams } from "./data/protocols/http";
+import { RemoteLoadUserList } from "./data/usecases/load-user-list/load-user-list";
 import { AxiosHttpClientAdapter } from "./infra/adapters";
-import { getUserList } from "./service/get-users";
+
 
 export function App() {
+  const axiosHttpClientAdapter = new AxiosHttpClientAdapter()
+  
+  const params: HttpRequestParams = {
+    url: 'https://jsonplaceholder.typicode.com/users',
+    method: 'get'
+  }
+
+  const remoteLoadUserList = new RemoteLoadUserList(params, axiosHttpClientAdapter)
 
   return (
     <>
       <h1>Users</h1>
       <UserList 
-        loadUserList={getUserList(new AxiosHttpClientAdapter())} 
+        loadUserList={remoteLoadUserList} 
       />
     </>
   )
